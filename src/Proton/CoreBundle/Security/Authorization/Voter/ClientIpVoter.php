529ed952fdf92e0e0c7fb5b724c501f94ed0a761
Derek Stobbe <djstobbe@gmail.com>
2012-01-18 22:06:45 -0700

    Add IP blacklist voter

---
+++ src/Proton/CoreBundle/Security/Authorization/Voter/ClientIpVoter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Proton\CoreBundle\Security\Authorization\Voter;
+
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ClientIpVoter implements VoterInterface
+{
+
+    protected $container;
+    protected $ipBlacklist;
+
+    public function __construct(ContainerInterface $container, array $ipBlacklist = array())
+    {
+        $this->container = $container;
+        $this->ipBlacklist = $ipBlacklist;
+    }
+
+    public function supportsAttribute($attribute)
+    {
+        return true;
+    }
+
+    public function supportsClass($class)
+    {
+        return true;
+    }
+
+    public function vote(TokenInterface $token, $object, array $attributes)
+    {
+        $request = $this->container->get('request');
+        if (in_array($request->getClientIp(), $this->ipBlacklist)) {
+            return VoterInterface::ACCESS_DENIED;
+        }
+
+        return VoterInterface::ACCESS_ABSTAIN;
+    }
+
+}
